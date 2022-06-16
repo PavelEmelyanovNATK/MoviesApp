073347d98b99540c3edcd694324a5b281d19517b
Pavel Emelyanov <p.emelyanov@mail.ru>
2022-06-16 16:55:52 +0700

    Added base ui

--- app/src/main/java/com/emelyanov/moviesapp/modules/movieslist/presentation/MoviesListFragment.kt
+++ app/src/main/java/com/emelyanov/moviesapp/modules/movieslist/presentation/MoviesListFragment.kt
@@ -16,6 +16,7 @@ class MoviesListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentMoviesListBinding.inflate(inflater, container, false)
+
         return binding.root
     }
 }
